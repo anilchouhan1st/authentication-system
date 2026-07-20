5ff0ab87a1b1403c8fe31b07f213a53423ef9115
Anil <anilchouhan1st@gmail.com>
2026-07-20 14:35:03 +0530

    Feat: Add 404 and 500 error pages and rename toast to custom-toast to avoid conflict with bootstrap

--- app.js
+++ app.js
@@ -38,10 +38,37 @@ app.use('/auth',require('./routes/auth'));
 const helmet = require("helmet");
 app.use(helmet());
 
+hbs.registerHelper("eq", function(a, b) {
+    return a === b;
+});
+
+
 // app.use(express.static('public'));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/test-error", (req, res) => {
+
+    throw new Error("Testing 500 Page");
+
+});
+
+app.use((req, res) => {
+    res.status(404).render("404", {
+        title: "404 - Page Not Found"
+    });
+});
+
+app.use((err, req, res, next) => {
+
+    console.error(err);
+
+    res.status(500).render("500", {
+        title: "500 - Internal Server Error"
+    });
+
 });
--- public/js/toast.js
+++ public/js/toast.js
@@ -1,58 +1,5 @@
-
-// function showToast(type, title, message) {
-
-//     console.log(type, title, message);
-
-//     const toast = document.querySelector(".toast");
-//     const icon = toast.querySelector(".icon");
-//     const text1 = toast.querySelector(".text-1");
-//     const text2 = toast.querySelector(".text-2");
-//     const progress = toast.querySelector(".progress");
-
-//     toast.classList.remove("success", "error", "warning", "info");
-//     toast.classList.add(type);
-
-//     text1.textContent = title;
-//     text2.textContent = message;
-
-// if (type === "success") {
-//     icon.className = "icon fa-solid fa-check";
-// } else if (type === "error") {
-//     icon.className = "icon fa-solid fa-xmark";
-// } else if (type === "warning") {
-//     icon.className = "icon fa-solid fa-triangle-exclamation";
-// } else {
-//     icon.className = "icon fa-solid fa-circle-info";
-// }
-
-//     toast.classList.add("active");
-//     progress.classList.add("active");
-
-//     setTimeout(() => {
-//         toast.classList.remove("active");
-//     }, 5000);
-
-//     setTimeout(() => {
-//         progress.classList.remove("active");
-//     }, 5300);
-
-//     const closeIcon = document.querySelector(".close");
-
-//     closeIcon.onclick = () => {
-//         toast.classList.remove("active");
-//         progress.classList.remove("active");
-//     };
-
-//     console.log(toast);
-//     console.log(text1.textContent);
-// console.log(text2.textContent);
-// console.log(toast.className);
-// }
-
-// console.log("Toast script loaded successfully");
-
 function showToast(type, title, message) {
-    const toast = document.querySelector(".toast");
+    const toast = document.querySelector(".custom-toast");
     const icon = toast.querySelector(".icon");
     const text1 = toast.querySelector(".text-1");
     const text2 = toast.querySelector(".text-2");
