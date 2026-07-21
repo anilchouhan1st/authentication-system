01b20d6db789ba09e706ad976a1203035a5c4e93
Anil <anilchouhan1st@gmail.com>
2026-07-21 21:57:41 +0530

    . Use explicit SMTP settings for testing

--- config/mail.js
+++ config/mail.js
@@ -14,15 +14,15 @@ dns.setDefaultResultOrder("ipv4first");
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
-
-// module.exports = transporter;
-
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
 transporter.verify((error, success) => {
     if (error) {
         console.error("SMTP Error:", error);
